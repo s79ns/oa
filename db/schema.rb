1b1bde94ba2b4ab9285d0a5c3ccfd4f7e9784bd3
s79ns <67348810+s79ns@users.noreply.github.com>
2020-08-15 14:26:17 +0900

    post
    ・postモデル・ルーティング・コントローラー・マイグレーション編集
    ・ログイン後のヘッダー表示のビュー修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_001201) do
+ActiveRecord::Schema.define(version: 2020_08_15_015531) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
