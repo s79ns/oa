411ea87ca4de460b51eb8e0837e7d81fdb2b63ba
s79ns <67348810+s79ns@users.noreply.github.com>
2020-08-20 11:47:14 +0900

    update定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
   private
   def post_params
     params.require(:post).permit(:name, :image, :text).merge(user_id: current_user.id)
