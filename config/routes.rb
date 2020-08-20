29dd2f3b051782acbf218fe74513534043a4e2cc
s79ns <67348810+s79ns@users.noreply.github.com>
2020-08-20 11:38:27 +0900

    updateルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'toppages/index'
   root "toppages#index"
-  resources :posts, only: [:index, :new, :create, :destroy, :edit]
+  resources :posts, only: [:index, :new, :create, :destroy, :edit, :update]
 end
