df79d489cc057bd82000b8697bf6550410d2266c
s79ns <67348810+s79ns@users.noreply.github.com>
2020-08-21 09:49:13 +0900

    ルーティング・モデル編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
+
+  def self.search(search)
+    if search != ""
+      Post.where('text LIKE(?)', "%#{search}")
+    else
+      Post.all
+    end
+  end
 end
