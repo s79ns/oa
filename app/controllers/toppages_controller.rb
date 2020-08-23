4f72069167797d83449d729da2fa4751f51107fd
s79ns <67348810+s79ns@users.noreply.github.com>
2020-08-23 12:32:24 +0900

    Update toppages_controller.rb

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,5 +4,11 @@ class ToppagesController < ApplicationController
   end
 
   def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+  end
+  sign_in user
+  redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end
