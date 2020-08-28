340c3381eeac310f3632b22a5b296abea0dd1251
s79ns <67348810+s79ns@users.noreply.github.com>
2020-08-28 09:42:41 +0900

    passwords.controller.rb作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'registrations'
-    passwords: 'registrations'
+    registrations: 'registrations',
+    passwords: 'passwords'
   }
   get 'toppages/index'
   root "toppages#index"
