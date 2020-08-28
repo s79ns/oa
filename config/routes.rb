e6ec8b0f6b249aa6040001b78ecc27c0854c7fe6
s79ns <67348810+s79ns@users.noreply.github.com>
2020-08-28 09:37:47 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'registrations'
+    passwords: 'registrations'
   }
   get 'toppages/index'
   root "toppages#index"
