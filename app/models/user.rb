28ce98ece7430850c56e37566acf49f6e1f57912
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-03 16:11:47 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  validates :nickname, uniqueness: true
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
