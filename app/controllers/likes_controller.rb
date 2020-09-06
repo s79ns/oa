93724a0aa09d5ffac06599483cb85676c50b7845
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-06 09:24:14 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,12 @@
 class LikesController < ApplicationController
   before_action :set_post
+
+  def create
+    @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+    @likes = Like.where(post_id: params[:post_id])
+    @post.reload
+  end
+
   private
 
   def set_post
