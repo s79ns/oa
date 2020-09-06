f468b9bee4847acc69ee18ddc0979166988e85c8
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-06 09:20:48 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :likes, dependent: :destroy
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
