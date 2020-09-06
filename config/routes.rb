70fc2b8f92835a1532927974b573c7c5fe0b39eb
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-06 09:25:45 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root "toppages#index"
   resources :posts do
     resources :posts, only: [:index, :new, :create, :destroy, :edit, :update]
+    resources :likes, only: [:create, :destroy]
   end
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users#new_guest'
