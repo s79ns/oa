49551cba49b78d64d99bf4ecc7b691d117d44e17
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-08 04:54:44 +0900

    Update user.rb
    いいね！後、ユーザー削除するとエラーが起きる不具合の修正。
    以下、エラー詳細：ActiveRecord::StatementInvalid: Mysql2::Error: Cannot delete or update a parent row: a foreign key constraint fails

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
-  has_many :likes
+  has_many :likes,foreign_key: :user_id, dependent: :destroy
   validates :nickname, presence: true
   validates :nickname, uniqueness: true
 
