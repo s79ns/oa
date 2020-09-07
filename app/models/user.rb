4776fdc4954ad585384ad7360b41aa2771a75353
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-08 05:49:30 +0900

    Update user.rb
    DBが空の状態でゲストユーザーを登録するとエラーが発生する問題を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
+      user.nickname = "ゲストユーザー"
       user.password = SecureRandom.urlsafe_base64
     end
   end
