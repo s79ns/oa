462f41087d66b8007da0694644bdc0fb5305f578
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-13 05:31:25 +0900

    Update pagetop.js

--- app/assets/javascripts/pagetop.js
+++ app/assets/javascripts/pagetop.js
@@ -1,8 +1,30 @@
-$(window).scroll(function () {
-  var now = $(window).scrollTop();
-  if (now > 200) {
-    $(".pagetop").fadeIn("slow");
-  } else {
-    $(".pagetop").fadeOut("slow");
-  }
+$(function () {
+  var showFlag = false;
+  var topBtn = $(".pagetop");
+  topBtn.css("bottom", "-100px");
+  var showFlag = false;
+  //スクロールが100に達したらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      if (showFlag == false) {
+        showFlag = true;
+        topBtn.stop().animate({ bottom: "20px" }, 200);
+      }
+    } else {
+      if (showFlag) {
+        showFlag = false;
+        topBtn.stop().animate({ bottom: "-100px" }, 200);
+      }
+    }
+  });
+  //スクロールしてトップ
+  topBtn.click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+    return false;
+  });
 });
