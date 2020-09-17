189580e81584951b2466ca9d554f3998db1d6df3
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-17 14:40:05 +0900

    ルートを変えると大変だよ('A`)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
+    redirect_to toppages_index_path, notice: 'ゲストユーザーとしてログインしました'
   end
 end
