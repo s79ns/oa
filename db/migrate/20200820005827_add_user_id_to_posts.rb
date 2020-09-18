012f3101202e3ccfac173d58c8cfa6d673e123df
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-19 05:48:09 +0900

    Update 20200820005827_add_user_id_to_posts.rb

--- db/migrate/20200820005827_add_user_id_to_posts.rb
+++ db/migrate/20200820005827_add_user_id_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPosts < ActiveRecord::Migration[6.0]
   def change
-    add_column :posts, :user_id, :integer
+    add_column :posts, :user_id, :references, null: false, foreign_key: true
   end
 end
