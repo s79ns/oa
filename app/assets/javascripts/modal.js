ebf1e6e18cef5b5caedf4567142a48588b01f22f
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-26 10:24:11 +0900

    モーダル仮実装
    せんせーありがとー・゜・(つД｀)・゜・

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,8 +1,18 @@
-$(function () {
-  $("#openModal").click(function () {
-    $("#modalArea").fadeIn();
-  });
-  $("#closeModal , #modalBg").click(function () {
-    $("#modalArea").fadeOut();
-  });
+// $(function () {
+//   $("#openModal").click(function () {
+//     $("#modalArea").fadeIn();
+//   });
+//   $("#closeModal , #modalBg").click(function () {
+//     $("#modalArea").fadeOut();
+//   });
+// });
+
+document.getElementById("modalOpen").addEventListener("click", function () {
+  document.getElementById("modal").classList.add("active");
+  document.getElementById("mask").classList.add("active");
+});
+
+document.getElementById("modalClose").addEventListener("click", function () {
+  document.getElementById("modal").classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
 });
