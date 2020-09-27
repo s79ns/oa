671b5de39609dc64fed0bb748ddf62d85e739bec
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-27 14:23:55 +0900

    関数化

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,18 +1,32 @@
-// $(function () {
-//   $("#openModal").click(function () {
-//     $("#modalArea").fadeIn();
-//   });
-//   $("#closeModal , #modalBg").click(function () {
-//     $("#modalArea").fadeOut();
-//   });
+// document.getElementById("modalOpen").addEventListener("click", function () {
+//   document.getElementById("modal").classList.add("active");
+//   document.getElementById("mask").classList.add("active");
 // });
 
-document.getElementById("modalOpen").addEventListener("click", function () {
-  document.getElementById("modal").classList.add("active");
-  document.getElementById("mask").classList.add("active");
-});
+// document.getElementById("modalClose").addEventListener("click", function () {
+//   document.getElementById("modal").classList.remove("active");
+//   document.getElementById("mask").classList.remove("active");
+// });
 
-document.getElementById("modalClose").addEventListener("click", function () {
-  document.getElementById("modal").classList.remove("active");
-  document.getElementById("mask").classList.remove("active");
-});
+// document.getElementById("mask").addEventListener("click", function () {
+//   document.getElementById("modal").classList.remove("active");
+//   document.getElementById("mask").classList.remove("active");
+// });
+
+(function () {
+  const modalOpen = document.getElementById("modalOpen");
+  const modalClose = document.getElementById("modalClose");
+  const mask = document.getElementById("mask");
+  const toggle = [modalOpen, modalClose, mask];
+
+  for (let i = 0, len = toggle.length; i < len; i++) {
+    toggle[i].addEventListener(
+      "click",
+      function () {
+        modal.classList.toggle("active");
+        mask.classList.toggle("active");
+      },
+      false
+    );
+  }
+})();
