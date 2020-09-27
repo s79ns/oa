61b20c9325b0e677e5b2d77d91d4aeba9d52df9f
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-27 20:20:11 +0900

    Update modal.js

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,18 +1,3 @@
-// document.getElementById("modalOpen").addEventListener("click", function () {
-//   document.getElementById("modal").classList.add("active");
-//   document.getElementById("mask").classList.add("active");
-// });
-
-// document.getElementById("modalClose").addEventListener("click", function () {
-//   document.getElementById("modal").classList.remove("active");
-//   document.getElementById("mask").classList.remove("active");
-// });
-
-// document.getElementById("mask").addEventListener("click", function () {
-//   document.getElementById("modal").classList.remove("active");
-//   document.getElementById("mask").classList.remove("active");
-// });
-
 (function () {
   const modalOpen = document.getElementById("modalOpen");
   const modalClose = document.getElementById("modalClose");
@@ -30,3 +15,8 @@
     );
   }
 })();
+
+// 即時関数 関数定義と同時実行し、関数範囲内のスコープを汚さずに新しいスコープを作ること。関数名は不要で、再利用しない一連の処理で使われる。
+// 変数 const(再代入・再定義不可) > let(再代入のみ可) > var(再代入・再定義可)
+// for (let i = 0, len = 変数.length; i < len; i++){} 配列の長さを変数にキャッシュし、配列の長さの取得を省略する。
+// toggle 指定したクラスの有無を識別し、クラスの追加及び削除を行う。
