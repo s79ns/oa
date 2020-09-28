b1b6d6f0e25c83295f6086f2a94cecdb236603bd
s79ns <67348810+s79ns@users.noreply.github.com>
2020-09-29 07:10:04 +0900

    ダメージ表示と終了フラグ

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,11 +1,11 @@
 const enemyData = {
   name: "ねんれいとみけいけんのかべ",
-  hp: 999999999999999,
+  hp: 100,
 };
 
 const playerData = {
   name: "エンジニアになりたいむしょく",
-  hp: 100,
+  hp: 30,
 };
 
 function insertText(id, text) {
@@ -21,5 +21,22 @@ insertText("PlayerHp", playerData["hp"]);
 insertText("currentPlayerHp", playerData["hp"]);
 
 document.getElementById("attack").addEventListener("click", function () {
-  alert("ミス！" + playerData["name"] + "のポートフォリオは見向きもされない！");
+  let endGame = false; //ゲーム完了フラグ
+
+  enemyData["hp"] -= 10;
+  playerData["hp"] -= 10;
+  insertText("currentEnemyHp", enemyData["hp"]);
+  insertText("currentPlayerHp", playerData["hp"]);
+
+  if (enemyData["hp"] <= 0) {
+    alert("勝ったッ！第三部完ッ！！");
+    endGame = true;
+  } else if (playerData["hp"] <= 0) {
+    alert("再起不能 -リタイヤ-");
+    endGame = true;
+  }
+
+  if (endGame) {
+    this.classList.add("deactive");
+  }
 });
