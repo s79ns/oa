d2db77de6dd55ed6eafc23cb058431a5057c09c9
s79ns <67348810+s79ns@users.noreply.github.com>
2020-10-04 09:53:27 +0900

    タブメニューjsの完成

---
+++ app/assets/javascripts/tabmenu.js
@@ -0,0 +1,13 @@
+const tabs = document.querySelectorAll(".tabmenu-container__tabs li a");
+const contents = document.querySelectorAll(".tabmenu-container__contents li");
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function () {
+    for (let j = 0; j < tabs.length; j++) {
+      tabs[j].classList.remove("active");
+      contents[j].classList.remove("active");
+    }
+    this.classList.add("active");
+    contents[i].classList.add("active");
+  });
+}
