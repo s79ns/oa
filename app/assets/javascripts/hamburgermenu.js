7047783dfc033cd0e68587c0c44974114eaf8515
s79ns <67348810+s79ns@users.noreply.github.com>
2020-10-07 09:54:16 +0900

    activeクラスの付け替え調整・メニュー表示非表示

--- app/assets/javascripts/hamburgermenu.js
+++ app/assets/javascripts/hamburgermenu.js
@@ -9,5 +9,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("menuButton").addEventListener("click", function () {
     this.classList.toggle("active");
+    document.getElementById("hamburger-nav").classList.toggle("active");
   });
 });
